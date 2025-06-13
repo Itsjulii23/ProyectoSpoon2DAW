7fccb07806271ab0b1372e3e6b203502dca5c503
Itsjulii23 <131156576+Itsjulii23@users.noreply.github.com>
2025-06-13 10:49:59 +0000

    Update Changes

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => '__root__',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => '3f05d10aab0445272814c74e45519f56261a8617',
+        'reference' => '92f68e0e65b790285edc51e9e97e16512a82eeea',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         '__root__' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => '3f05d10aab0445272814c74e45519f56261a8617',
+            'reference' => '92f68e0e65b790285edc51e9e97e16512a82eeea',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
